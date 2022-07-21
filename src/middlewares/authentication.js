4754daa38cdf558bb194aaaf7fc0a12f986d4560
leonardo-terra <leo.terra.melo@gmail.com>
2022-07-21 15:43:24 -0300

    refactor: Remove a palavra Bearer, caso ela esteja presente no token

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,7 +2,10 @@ const { authenticateToken } = require('../utils/JWT');
 
 const authentication = async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    let token = req.headers.authorization;
+    if (token.startsWith('Bearer ')) {
+      token = token.substring(7, token.length);
+    }
     const payload = await authenticateToken(token);
     if (!payload) throw Error('Token not found');
     res.locals.payload = payload;
