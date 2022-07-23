aa0d26080fd81d067eb0b07903537737d9ccbe45
leonardo-terra <leo.terra.melo@gmail.com>
2022-07-23 11:41:52 -0300

    doc: Atualiza documentação com a padronização feita

--- src/validations/dataValidationJoi.js
+++ src/validations/dataValidationJoi.js
@@ -1,14 +1,28 @@
 const Joi = require('joi');
 
-const buyAndSalesSchema = Joi.object().keys({
-  codCliente: Joi.number().required(),
-  codAtivo: Joi.number().required(),
-  qntdeAtivo: Joi.number().min(1).required(),
-});
+const buyAndSalesSchema = Joi.object()
+  .keys({
+    codCliente: Joi.number().required(),
+    codAtivo: Joi.number().required(),
+    qntdeAtivo: Joi.number().min(1).required(),
+  })
+  .required()
+  .messages({
+    'any.required': '{{#label}} é de preenchimento obrigatório.',
+    'number.base': '{{#label}} precisa ser um número inteiro.',
+    'number.min': '{{#label}} precisa ser maior que zero.',
+  });
 
-const depositAndWithdrawSchema = Joi.object().keys({
-  codCliente: Joi.number().required(),
-  valor: Joi.number().min(1).required(),
-});
+const depositAndWithdrawSchema = Joi.object()
+  .keys({
+    codCliente: Joi.number().required(),
+    valor: Joi.number().min(1).required(),
+  })
+  .required()
+  .messages({
+    'any.required': '{{#label}} é de preenchimento obrigatório.',
+    'number.base': '{{#label}} precisa ser um número inteiro.',
+    'number.min': '{{#label}} precisa ser maior que zero.',
+  });
 
-module.exports = { buyAndSalesSchema, depositAndWithdrawSchema }
+module.exports = { buyAndSalesSchema, depositAndWithdrawSchema };
